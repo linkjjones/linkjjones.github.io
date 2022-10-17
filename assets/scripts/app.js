7adc1ecc043760310a2a4e794b31032bcb02ff2e
BlackThread <blackthread.dev@gmail.com>
2022-10-17 00:18:41 -0400

    updates entire site

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,4 @@
-const Portfolio = function() {
+const Portfolio = function () {
 	function makeWords() {
 		var words = [
 			{
@@ -34,12 +34,12 @@ const Portfolio = function() {
 	}
 
 	function makeWordCloud(words) {
-		$('.teaching-domains').jQCloud(words, {delay: 120});
+		$('.teaching-domains').jQCloud(words, { delay: 120 });
 	}
 
 	function displayWordCloud() {
 		var count = 1;
-		$(window).on('scroll', function() {
+		$(window).on('scroll', function () {
 			var y_scroll_pos = window.pageYOffset;
 			var scroll_pos_test = 2700; // set to whatever you want it to be
 			var words = makeWords();
@@ -57,18 +57,21 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+				"am a Software Engineer.",
+				"love tech that helps people.",
+				"also learning different technologies.",
+				"solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
 			// typing speed
 			typeSpeed: 1,
 			contentType: 'text',
-			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			callback: function () {
+				$("#writing-text").css({ "color": "#fff", "background-color": "#C8412B" });
 			},
-			preStringTyped: function() {},
-			onStringTyped: function() {}
+			preStringTyped: function () { },
+			onStringTyped: function () { }
 		});
 	}
 
